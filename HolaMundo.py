d0f718b525d489dd6513d080a41e9f723ca367fe
Tomás <tomaspitavino@gmail.com>
2025-08-20 20:53:57 -0300

    commit edited

--- HolaMundo.py
+++ HolaMundo.py
@@ -1,6 +1,1 @@
-n=int{input={"Enter a number of rows"}}
-for i in range(n):
-    for j in range(4):
-        print("*" , end=" ")
-    print()
-    
+print("Hello, World!")
--- pruebas.py
+++ pruebas.py
@@ -2,6 +2,6 @@ print ("\x1b[1;33m"+"|---------------------|")
 print ("\x1b[1;33m"+"|------ Pruebas ------|")
 print ("\x1b[1;33m"+"|---------------------|")
 
-
 x = "10"
 print (x * 3 + "5")
+print("\033[33mEste texto es amarillo\033[0m")
