a31a6fe8acac2203e2a68291ddbc5a934b4c1da8
Tomás <tomaspitavino@gmail.com>
2025-08-21 09:52:53 -0300

    tipos de datos

---
+++ 03-Aritmeticas.py
@@ -0,0 +1,23 @@
+#"""
+#    Realiza operaciones aritméticas básicas.
+#"""
+
+#print (5+2)
+#print (5-2)
+#print (5*2)
+#print (5%2)
+#print (5 ** 2)
+
+#comparaciones
+#n1 = 10
+#n2 = 15
+#print (n1 > n2)
+#print (n1 < n2)
+#print (n1 >= n2)
+#print (n1 <= n2)
+#print (n2 == n2)
+
+#Datos de usuario - input
+
+resultado = input("Ingresa tu edad: ")
+print(type(resultado))
