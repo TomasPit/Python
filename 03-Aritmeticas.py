748aa9d7d03529618a3de54308509c079cfad29c
Tomás <tomaspitavino@gmail.com>
2025-08-21 13:15:02 -0300

    commit until 09-conversor de °C a °F

--- 03-Aritmeticas.py
+++ 03-Aritmeticas.py
@@ -17,7 +17,31 @@
 #print (n1 <= n2)
 #print (n2 == n2)
 
-#Datos de usuario - input
+#Datos de usuario - input y conversion de archivos
 
-resultado = input("Ingresa tu edad: ")
-print(type(resultado))
+#resultado = int(input("Ingresa tu edad: "))
+#print(type(resultado))
+#print ("Tu edad es", resultado , "y si le sumo dos es: ", resultado + 2)
+
+#Control de flujo - IF
+
+#nota = int(input("Ingresa tu nota: "))
+#if nota > 95:
+#    print("Aprobado con honores")
+#elif nota == 95:
+#    print("Aprobado con lo justo")
+#else:
+#    print("Desaprobado, la proxima será")
+
+#conversor de temperaturas 
+
+temperatura = float(input("Ingrese la temperatura a convertir: "))
+
+caracter = input("Ingrese 'C' para convertir a Celsius o 'F' para convertir a Fahrenheit: ")
+
+if  caracter == 'F' or caracter == 'f':
+    temperatura = temperatura + 273
+    print("La temperatura en farenheit es: ", temperatura, "°F")
+else:
+    print("La temperatura es: ", temperatura, "°C")
+
